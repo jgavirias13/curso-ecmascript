eec8a430d9fec82332550945415f3e8f4e3176b3
Juan Pablo Gaviria <juan.gaviria@gorillalogic.com>
2022-10-18 17:51:02 -0500

    Spread operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -14,3 +14,25 @@ let user = {
 
 let {username, fullName} = user;
 console.log(username, fullName);
+
+// Spread operator
+
+let person = {
+  name: 'Juan',
+  age: 28
+};
+
+let country = 'CO';
+
+let data = {id: 1, ...person, country}; // Se realiza la desestructuración de person
+
+console.log(data);
+
+// Rest parameters
+
+function sum(num, ...values) { //Values sera una serie de parametros
+  console.log(values);
+}
+
+sum(1);
+sum(1,2,3,4,5);
